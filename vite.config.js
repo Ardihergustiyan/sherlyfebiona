b6616a4eebba38b34abd47b005792aaa63d13c08
Ardi Hergustiyan <hergustiyan@gmail.com>
2025-12-24 13:09:17 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/my-app/', // Ganti 'my-app' dengan nama repository kamu
+  base: '/sherifebiona/', // Nama repository kamu
 })
