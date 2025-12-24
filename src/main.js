fd30fdd464cb1274864a66f6040bfbe890b1a467
Ardi Hergustiyan <hergustiyan@gmail.com>
2025-12-24 13:03:13 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-2xl">
+      <h1 class="text-4xl font-bold text-gray-800 mb-4">Hello World!</h1>
+    </div>
+  </div>
+`
